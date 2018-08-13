9dfaa1306901b7dc677bed96c63d7bb4b0ae383a
kazlauskis <karolis@kazlauskis.com>
2018-08-13 20:53:56 +0100

    Fix gps ref

--- src/common/models/sample_geoloc_ext.js
+++ src/common/models/sample_geoloc_ext.js
@@ -3,27 +3,54 @@
  *
  * Sample geolocation events:
  * start, update, error, success, stop
- *****************************************************************************/
+ **************************************************************************** */
 import $ from 'jquery';
-import GPS from 'helpers/gps';
+import GPS from 'helpers/GPS';
 import Log from 'helpers/log';
 import LocHelp from 'helpers/location';
 import appModel from 'app_model';
+
+export function updateSampleLocation(sample, location) {
+  return new Promise(resolve => {
+    const newLocation = Object.assign({}, location);
+    newLocation.source = 'gps';
+    newLocation.updateTime = new Date(); // track when gps was acquired
+    newLocation.gridref = LocHelp.locationToGrid(newLocation);
+
+    // extend old location to preserve its previous attributes like name or id
+    const oldLocation = sample.get('location');
+    const fullLocation = $.extend(oldLocation, newLocation);
+
+    if (sample.setGPSLocation) {
+      const locationIsUpdatedPromise = sample.setGPSLocation(fullLocation);
+      if (locationIsUpdatedPromise) {
+        locationIsUpdatedPromise.then(() => resolve(true));
+      } else {
+        resolve(false);
+      }
+      return;
+    }
+
+    sample.set('location', fullLocation);
+    sample.save().then(() => resolve(true));
+  });
+}
 
 const extension = {
   startGPS(accuracyLimit) {
     Log('SampleModel:GPS: start.');
 
+    // eslint-disable-next-line
     const that = this;
     const options = {
       accuracyLimit,
+
       onUpdate(location) {
         that.trigger('geolocation', location);
         that.trigger('geolocation:update', location);
       },
 
-      callback(error, loc) {
-        let location = loc;
+      callback(error, location) {
         extension.stopGPS.call(that, { silent: true });
 
         if (error) {
@@ -32,26 +59,24 @@ const extension = {
           return;
         }
 
-        location.source = 'gps';
-        location.updateTime = new Date(); // track when gps was acquired
-        location.gridref = LocHelp.locationToGrid(location);
-
-        // extend old location to preserve its previous attributes like name or id
-        const oldLocation = that.get('location');
-        location = $.extend(oldLocation, location);
-
-        that.set('location', location);
-        that.save();
-
-        that.trigger('change:location');
-        that.trigger('geolocation', location);
-        that.trigger('geolocation:success', location);
+        updateSampleLocation(that, location)
+          .then(locationWasSet => {
+            if (locationWasSet) {
+              that.trigger('change:location');
+              that.trigger('geolocation', location);
+              that.trigger('geolocation:success', location);
+            }
+          })
+          .catch(() => {
+            // todo: return err
+            that.trigger('geolocation:error', location);
+          });
       },
     };
 
     this.locating = GPS.start(options);
-    that.trigger('geolocation');
-    that.trigger('geolocation:start');
+    this.trigger('geolocation');
+    this.trigger('geolocation:start');
   },
 
   stopGPS(options = {}) {
