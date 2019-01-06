f435b062b1cb3225c5e079802de2c909c024af39
kazlauskis <karolis@kazlauskis.com>
2019-01-06 23:35:11 +0000

    Fix species sorting

--- src/common/species_list_sorts.js
+++ src/common/species_list_sorts.js
@@ -8,11 +8,11 @@ import appModel from 'app_model';
  * id - sort type identifier
  * label - label to represent the filter in the UI
  */
-function exist(country, a, b) {
-  if (!a.attributes[country].exist === 'YES') {
+function exist(country, species1, species2) {
+  if (!species1.attributes[country].exist === 'YES') {
     return false;
   }
-  if (!b.attributes[country].exist === 'YES') {
+  if (!species2.attributes[country].exist === 'YES') {
     return false;
   }
 
@@ -20,74 +20,81 @@ function exist(country, a, b) {
 }
 
 const sorts = {
-  default(a, b) {
+  default(species1, species2) {
     const country = appModel.get('country');
-    if (!exist(country, a, b)) {
+    if (!exist(country, species1, species2)) {
       return 1;
     }
-    a = a.attributes[country].order;
-    b = b.attributes[country].order;
+    let commonName1 = species1.attributes[country].order;
+    let commonName2 = species2.attributes[country].order;
 
-    if (a === b) {
+    if (commonName1 === commonName2) {
       return 0;
     }
-    return a > b ? 1 : -1;
+    return commonName1 > commonName2 ? 1 : -1;
   },
-  common(a, b) {
+  common(species1, species2) {
     const country = appModel.get('country');
-    if (!exist(country, a, b)) {
-      return 1;
-    }
-
-    if (!a.attributes[country].common_name) return 1;
-    if (!b.attributes[country].common_name) return -1;
-
-    a = a.attributes[country].common_name.toLowerCase();
-    b = b.attributes[country].common_name.toLowerCase();
-
-    if (a === b) {
-      return 0;
-    }
-    return a > b ? 1 : -1;
-  },
-  'common-reverse'(a, b) {
-    const country = appModel.get('country');
-    if (!exist(country, a, b)) {
+    if (!exist(country, species1, species2)) {
       return 1;
     }
 
-    if (!a.attributes[country].common_name) return 1;
-    if (!b.attributes[country].common_name) return -1;
+    let commonName1 = species1.attributes[country].common_name;
+    let commonName2 = species2.attributes[country].common_name;
+    if (!commonName1) return 1;
+    if (!commonName2) return -1;
 
-    a = a.attributes[country].common_name.toLowerCase();
-    b = b.attributes[country].common_name.toLowerCase();
+    if( !isNaN(commonName1[0]) && isNaN(commonName2[0])){
+      return 1;
+    }  
+    if( !isNaN(commonName2[0]) && isNaN(commonName1[0])){
+      return -1;
+    }  
 
-    if (a === b) {
-      return 0;
-    }
-    return a < b ? 1 : -1;
+    return commonName1.localeCompare(commonName2, undefined, {numeric: true});
   },
-  scientific(a, b) {
-    const a_taxon = a.attributes.taxon.toLowerCase();
-    const b_taxon = b.attributes.taxon.toLowerCase();
-
-    if (a_taxon === b_taxon) {
-      const a_form = a.attributes.form;
-      const b_form = b.attributes.form;
-      return a_form > b_form;
+  'common-reverse'(species1, species2) {
+    const country = appModel.get('country');
+    if (!exist(country, species1, species2)) {
+      return 1;
     }
-    return a_taxon > b_taxon ? 1 : -1;
+    let commonName1 = species1.attributes[country].common_name;
+    let commonName2 = species2.attributes[country].common_name;
+    if (!commonName1) return 1;
+    if (!commonName2) return -1;
+
+    if( !isNaN(commonName1[0]) && isNaN(commonName2[0])){
+      return -1;
+    }  
+    if( !isNaN(commonName2[0]) && isNaN(commonName1[0])){
+      return 1;
+    }  
+    
+    return commonName2.localeCompare(commonName1, undefined, {numeric: true});
   },
-  'scientific-reverse'(a, b) {
-    const a_taxon = a.attributes.taxon.toLowerCase();
-    const b_taxon = b.attributes.taxon.toLowerCase();
 
-    if (a_taxon === b_taxon) {
-      const a_form = a.attributes.form;
-      const b_form = b.attributes.form;
-      return a_form < b_form;
+  
+  scientific(species1, species2) {
+    const aTaxon = species1.attributes.taxon.toLowerCase();
+    const bTaxon = species2.attributes.taxon.toLowerCase();
+
+    if (aTaxon === bTaxon) {
+      const aForm = species1.attributes.form;
+      const bForm = species2.attributes.form;
+      return aForm > bForm;
     }
-    return a_taxon < b_taxon ? 1 : -1;
+    return aTaxon > bTaxon ? 1 : -1;
+  },
+  'scientific-reverse'(species1, species2) {
+    const aTaxon = species1.attributes.taxon.toLowerCase();
+    const bTaxon = species2.attributes.taxon.toLowerCase();
+
+    if (aTaxon === bTaxon) {
+      const aForm = species1.attributes.form;
+      const bForm = species2.attributes.form;
+      return aForm < bForm;
+    }
+    return aTaxon < bTaxon ? 1 : -1;
   },
 };
 
