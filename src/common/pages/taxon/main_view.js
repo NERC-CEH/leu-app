f6b9e74c1d5a6e2343b01046a009c6d3a65f69cf
Karolis Kazlauskis <karolis@flumens.io>
2020-03-10 19:23:46 +0000

    Fix empty species message translation

--- src/common/pages/taxon/main_view.js
+++ src/common/pages/taxon/main_view.js
@@ -1,25 +1,25 @@
 /** ****************************************************************************
  * Home main view.
  **************************************************************************** */
-import Marionette from "backbone.marionette";
-import _ from "lodash";
-import appModel from "app_model";
-import JST from "JST";
-import "./styles.scss";
+import Marionette from 'backbone.marionette';
+import _ from 'lodash';
+import appModel from 'app_model';
+import JST from 'JST';
+import './styles.scss';
 
 const SpeciesView = Marionette.View.extend({
-  tagName: "li",
-  className: "table-view-cell",
+  tagName: 'li',
+  className: 'table-view-cell',
 
-  template: JST["common/taxon/species"],
+  template: JST['common/taxon/species'],
 
   events: {
     // eslint-disable-next-line
-    "click a": function(e) {
+    'click a': function(e) {
       if (this.options.sampleModel) {
         e.preventDefault();
       }
-      this.trigger("select", this.model.attributes);
+      this.trigger('select', this.model.attributes);
     },
   },
 
@@ -27,37 +27,38 @@ const SpeciesView = Marionette.View.extend({
     const species = this.model;
     const commonName = appModel.getSpeciesLocalName(species);
 
-    const sort = this.options.appModel.get("sort");
+    const sort = this.options.appModel.get('sort');
     const sortScientific =
-      sort === "scientific" || sort === "scientificReverse";
+      sort === 'scientific' || sort === 'scientificReverse';
 
-    const form = species.get("form");
+    const form = species.get('form');
     return {
-      id: species.get("id"),
-      taxon: species.get("taxon"),
+      id: species.get('id'),
+      taxon: species.get('taxon'),
       form,
       commonName,
-      favourite: this.options.appModel.isFavouriteSpecies(species.get("id")),
+      favourite: this.options.appModel.isFavouriteSpecies(species.get('id')),
       sortScientific,
     };
   },
 });
 
 const NoSpeciesView = Marionette.View.extend({
-  tagName: "li",
-  className: "table-view-cell empty",
-  template: _.template(`<p>${t("No species with selected filters.")}</p>`),
+  tagName: 'li',
+  className: 'table-view-cell empty',
+  template: data =>
+    _.template(`<p>${t('No species with selected filters.')}</p>`)(data),
 });
 
 export default Marionette.CollectionView.extend({
-  id: "species-list",
-  tagName: "ul",
-  className: "table-view no-top",
+  id: 'species-list',
+  tagName: 'ul',
+  className: 'table-view no-top',
   childView: SpeciesView,
   emptyView: NoSpeciesView,
 
   initialize() {
-    this.listenTo(this.options.appModel, "change:filter", this.render);
+    this.listenTo(this.options.appModel, 'change:filter', this.render);
   },
 
   childViewOptions() {
